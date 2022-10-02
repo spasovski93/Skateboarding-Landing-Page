783f69c8e54bbdf212d2f803dc80f01b71c2397a
spasovski93 <102192904+spasovski93@users.noreply.github.com>
2022-10-02 21:27:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// This is my first js code //
+console.log('Skateboarding Blog');
+let firstName = 'Mario'; // String Literal
+let lastName = 'Spasovski'; // String Literal
+let age = 30; // Number Literal
